f697387112c2bea65e7b5c250c2cbc8c1b8147b2
evan4 <evan4mc@gmail.com>
2019-01-16 14:31:35 +0500

    add previous url to modal

--- src/components/Dialog.vue
+++ src/components/Dialog.vue
@@ -21,7 +21,7 @@
         </v-card-title>
 
         <v-card-text>
-          Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+          {{ prevRoute }}
         </v-card-text>
 
         <v-divider></v-divider>
@@ -42,11 +42,19 @@
 </template>
 
 <script>
+import router from '../router';
+
 export default {
   name: 'Dialog',
+  beforeRouteEnter (to, from, next) {
+     next(vm => {
+      vm.prevRoute = from.path
+    })
+  },
   data () {
     return {
-      dialog: true
+      dialog: true,
+      prevRoute: null
     }
   }
 }
